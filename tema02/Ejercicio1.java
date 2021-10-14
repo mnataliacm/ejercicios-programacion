2c4a919c751dff1d170d21af7801abd011c900a8
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-14 10:18:26 +0200

    Ejercicio1

---
+++ tema02/Ejercicio1.java
@@ -0,0 +1,19 @@
+/**
+ * Ejercicio 1, crear dos variables y sumar, restar, dividir y multiplicar
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    int x = 144;
+    int y = 999 ;
+    
+    System.out.println("La variable x = " + x);
+    System.out.println("La variable y = " + y);
+    System.out.println("La suma es = " + (x + y));
+    System.out.println("La resta es = " + (x - y));
+    System.out.println("La división es = " + (y / x));
+    System.out.println("La multiplicación es = " + (x * y));
+    
+  }
+}
