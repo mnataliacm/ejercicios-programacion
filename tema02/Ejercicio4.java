7bf089d79b1c901635073252eebc563e45e4edde
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-14 11:12:29 +0200

    ejercicio4

---
+++ tema02/Ejercicio4.java
@@ -0,0 +1,20 @@
+/**
+ * Ejercicio 4, crea un conversor de euros a pesetas.
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    
+    double euro = 15;
+    double peseta = 166.386;
+    double conversor = euro * peseta;
+        
+   // euro = 5;
+   // peseta = 166.386;
+   // conversor = (euro*peseta);
+    
+    System.out.println("La cantidad de: " + euro + " euros equivale a: " + conversor + " pesetas");
+    
+  }
+}
