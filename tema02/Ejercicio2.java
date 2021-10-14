aa859ff298f0ceb12f9760f4acad2d5645a2cc50
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-14 10:30:23 +0200

    Ejercicio2

---
+++ tema02/Ejercicio2.java
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 1, crear dos variables y sumar, restar, dividir y multiplicar
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    
+    String nombre = "Natalia Castillo Muñoz";
+    
+    System.out.println("Mi nombre es " + nombre);
+    
+  }
+}
