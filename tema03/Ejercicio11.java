c7f23816841fce043e0dde5769626f9b4513f528
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-19 11:10:30 +0200

    actualizando

---
+++ tema03/Ejercicio11.java
@@ -0,0 +1,21 @@
+/**
+ * Tema 3
+ * Ejercicio 11
+ * Programa que convierte de kB a MB y a MiB.
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio11 {
+  public static void main(String[] args) {
+    
+    System.out.println("---Convertir de kB a MB y a MiB---");
+    
+    System.out.print("Introduce los kilobytes a convertir: ");
+    double kilob = Double.parseDouble(System.console().readLine());
+    double MB = kilob / 1000;
+    double MiB = (double)kilob / 1024;
+    
+    System.out.println("El resultado es: " + MB + " MB y " + MiB + " MiB");
+    
+  }
+}
---
+++ tema03/Ejercicio12.java
@@ -0,0 +1,21 @@
+/**
+ * Tema 3
+ * Ejercicio 12
+ * 
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejerciciox {
+  public static void main(String[] args) {
+    
+    System.out.println("---CALCULAR NOTA MEDIA PONDERADA---");
+    
+    System.out.print("Introduce la nota del primer examen: ");
+    double examen = Double.parseDouble(System.console().readLine());
+    System.out.print("¿que nota quieres sacar en el trimestre?");
+    double notaTrimestre = Double.parseDouble(System.console().readLine());
+    
+    
+    
+  }
+}
