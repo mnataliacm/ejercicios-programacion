d3ce74d7999e73182aa4870a875dfd1ba6eed503
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-19 10:45:28 +0200

    actualizando

---
+++ tema03/Ejercicio10.java
@@ -0,0 +1,21 @@
+/**
+ * Tema 3
+ * Ejercicio 10
+ * Programa que convierte MB en kB.
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio10 {
+  public static void main(String[] args) {
+    
+    System.out.println("---Convertir de MB a kB y a kiB---");
+    
+    System.out.print("Introduce los megabytes a convertir: ");
+    int megas = Integer.parseInt(System.console().readLine());
+    int kB = megas * 1000;
+    int kiB = megas * 1024;
+    
+    System.out.println("El resultado es: " + kB + " kB y " + kiB + " kiB");
+    
+  }
+}
