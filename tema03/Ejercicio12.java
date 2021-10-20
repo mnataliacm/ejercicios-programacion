8d719a1d9a90f2e5b37fe449951a921a48733459
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-20 11:02:08 +0200

    ejercicios

--- tema03/Ejercicio12.java
+++ tema03/Ejercicio12.java
@@ -5,17 +5,19 @@
  *
  * @author Natalia Castillo Muñoz
  */
-public class Ejerciciox {
+public class Ejercicio12 {
   public static void main(String[] args) {
     
     System.out.println("---CALCULAR NOTA MEDIA PONDERADA---");
     
     System.out.print("Introduce la nota del primer examen: ");
     double examen = Double.parseDouble(System.console().readLine());
-    System.out.print("¿que nota quieres sacar en el trimestre?");
+    System.out.print("¿que nota quieres sacar en el trimestre? ");
     double notaTrimestre = Double.parseDouble(System.console().readLine());
     
+    double nota = ((notaTrimestre*100)-(examen*40))/60;
     
+    System.out.printf(" Para tener "+notaTrimestre+" necesitas sacar en el segundo examen un " +nota);
     
   }
 }
