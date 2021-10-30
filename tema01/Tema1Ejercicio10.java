e78ff2cad542ba9c648948588b2b505c9a1d57e8
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-10-30 21:52:43 +0200

    actualizo readme

--- tema01/Tema1Ejercicio10.java
+++ tema01/Tema1Ejercicio10.java
@@ -13,8 +13,8 @@ public class Tema1Ejercicio10 {
     System.out.println("");
     System.out.println("\033[93m      \u2654 \033[39;49m");
     System.out.println("      \u263A ");
-    System.out.println("     \u2571\033[96m\u2503\033[39;49m\u2572");
-    System.out.println("      \033[36m\u2551\033[39;49m");
+    System.out.println("      \u2571\033[96m\u2503\033[39;49m\u2572");
+    System.out.println("       \033[36m\u2551\033[39;49m");
     System.out.println("\033[32m \u2592\u2592\u2592\u2592\u2592\u2592\u2592\u2592\u2592\u2592\u2592\u2592 \033[39;49m");
     System.out.println("   ");
 
