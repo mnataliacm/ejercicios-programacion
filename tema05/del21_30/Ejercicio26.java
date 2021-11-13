25722e97e191efa3b412ba681f857c5f04fe5deb
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-11-13 13:05:10 +0100

    ejercicios Tema 05

---
+++ tema05/del21_30/Ejercicio26.java
@@ -0,0 +1,43 @@
+/**
+ * Tema 5
+ * Ejercicio 26
+ * Programa que pide un número de varios dígitos. Luego pide un dígito.
+ * Calcular la posicion o posiciones contando de izq a dere
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio26 {
+  public static void main(String[] args) {
+
+    System.out.println("___Programa que calcula la posición de un dígito___");
+    System.out.print("Introduce un número: ");
+    int numero = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduce un dígito: ");
+    int digito = Integer.parseInt(System.console().readLine());
+    //variables
+    int num = numero;
+    int cortar = 1;
+    //Contar dígitos
+    while (num != 0) {
+      num = num / 10;
+      cortar *= 10;
+    }
+    System.out.print("El dígito " + digito + " está en la/s posición/es: ");
+    cortar = cortar / 10;
+    num = numero / cortar;
+    int posicion = 1;
+    if (num == digito) {
+      System.out.print(posicion + ", ");
+    }
+    while (cortar >= 10) {
+      posicion++; 
+      cortar = cortar / 10;
+      num = (numero / cortar) % 10;
+      if (num == digito) {
+        System.out.print(posicion + ", ");
+      }
+    }
+    System.out.println();
+  }
+}
+
---
+++ tema05/del21_30/Ejercicio27.java
@@ -0,0 +1,35 @@
+/**
+ * Tema 5
+ * Ejercicio 27
+ * Programa que muestre, cuente y sume los múltiplos de 3.
+ * Desde 1 hasta numero introducido por teclado.
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio27 {
+  public static void main(String[] args) {
+
+    System.out.println("___Programa que muestra, cuenta y suma los múltiplos de 3___");
+    System.out.print("Introduce número de parada: ");
+    int parada = Integer.parseInt(System.console().readLine());
+    //variables
+    int multiplos = 0;
+    int conteo = 0;
+    int suma = 0;
+    //operaciones
+    System.out.println("\nLos múltiplos de 3 desde 1 hasta " + parada + " son: ");
+    for (int i = 1; i <= parada; i++) {
+      multiplos = i % 3;
+      if (multiplos == 0) {
+        System.out.print(i + ", ");
+        conteo++;
+        suma = suma + i;
+
+      }
+    }
+  System.out.println();
+  System.out.println("Total de multiplos de 3 encontrados: " + conteo);
+  System.out.println("La suma de todos los múltiplos de 3 es: " + suma);
+  }
+}
+
