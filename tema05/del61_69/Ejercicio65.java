9c9a9e5d9900b29309e9684d240d121674b96ab9
Natalia Castillo Muñoz <mnacamu@hotmail.com>
2021-12-20 20:00:09 +0100

    Ejercicios Tema 05

--- tema05/del61_69/Ejercicio65.java
+++ tema05/del61_69/Ejercicio65.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Tema 5
  * Ejercicio 65
@@ -9,69 +10,47 @@
  * @author Natalia Castillo Muñoz
  */
 public class Ejercicio65 {
-  public static void main(String[] args) {
-
-    System.out.println();
-    System.out.println("___Programa que pinta una A___");
-    //variables
-    int altura = 1;
-    int fila = 1;
-    int linea = 0;
-    String caracter = "*";
-    //pedir datos
-    do {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
       System.out.print("Introduzca la altura de la A (un número mayor o igual a 3): ");
-      altura = Integer.parseInt(System.console().readLine());
-      System.out.print("Introduzca la final donde quieres que vaya el palito horizontal ( entre 2 y 6): ");
-      fila = Integer.parseInt(System.console().readLine());
-      if ((altura < 3) && (fila < 2) || (fila > 6)) {
-        System.out.println();
-        System.out.println("Los datos introducidos son incorrectos, intentalo de nuevo.");
-        System.out.println();
-      }
-    } while ((altura < 3) && (fila < 2) || (fila > 6));
-    //espacios parte superior
-    System.out.println();
-    for (int espacio = 0; espacio < altura + 3; espacio++) { 
-      System.out.print(" ");
-    }
-    //pico de la A
-    System.out.print(caracter);
-    //resto de la A
-    for (linea = 0; linea <= altura; linea++) {
-      System.out.println();
-      for (int espacio = 1; espacio <= altura - linea + 2; espacio++) { 
-        System.out.print(" ");
-      }
-      System.out.print(caracter);
-      for (int hueco = 0; hueco <= (linea * 2); hueco++) { 
-        System.out.print(" ");
-      }
-      System.out.print(caracter);
-    }
-    System.out.println();
-    //espacios palito
-    for (int espacio = 0; espacio < 2; espacio++) { 
-      System.out.print(" ");
-    }
-    //palito horizontal
-    for (int dibujo = 0; dibujo <= (linea * 2) ; dibujo++) {
-        System.out.print(caracter);
-    }
-    //espacios parte inferior
-    System.out.println();
-    for (int espacio = 0; espacio < 2; espacio++) { 
-      System.out.print(" ");
-      //parte inferior
-      for (int i = 0; i < 2; i++) {
-        System.out.print(caracter);
-        for (espacio = 1; espacio <= (linea * 2); espacio++) { 
-          System.out.print(" ");
+      int altura = Integer.parseInt(s.nextLine());
+      if (altura < 3) {
+        System.out.println("La altura introducida no es correcta.");
+      } else {
+        System.out.print("Introduzca la fila del palito horizontal (entre 2 y " + (altura - 1) + "): ");
+        int palito = Integer.parseInt(s.nextLine());
+        
+        if ((palito < 2) || (palito >= altura)) {
+          System.out.println("La fila introducida no es correcta.");
+        } else {
+          System.out.println();
+          // Pinta la letra A
+          int espaciosIzq = altura - 1;
+          int espaciosCentro = 1;
+          
+          for (int fila = 1; fila <= altura; fila++) {      
+            repiteCaracter(" ", espaciosIzq--);
+            System.out.print("*");
+            if (fila > 1) {
+              if (fila == palito) {
+                repiteCaracter("*", espaciosCentro);
+              } else {
+                repiteCaracter(" ", espaciosCentro);
+              }
+              
+              espaciosCentro+=2;
+              System.out.print("*");
+            }
+            System.out.println();
+          }
+          
         }
-        System.out.println(caracter);
       }
     }
-    System.out.println();
+    
+    public static void repiteCaracter(String c, int n) {
+      for (int i = 0; i < n; i++) {
+        System.out.print(c);
+      }
   }
 }
-
