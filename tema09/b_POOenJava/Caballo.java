bd7f72f021da5edd577d4a12c97ce4cdfef90779
Natalia <Natalia@DESKTOP-928A3QC>
2022-02-08 22:03:19 +0100

    Ejercicios Tema 09

---
+++ tema09/b_POOenJava/Caballo.java
@@ -0,0 +1,45 @@
+package ejercicios.tema09.b_POOenJava;
+
+/**
+ * Caballo.java
+ * Implementar la clase Caballo
+ * @author Natalia
+ */
+public class Caballo {
+  //atributos
+  private String nombre;
+  private String color;
+  private String sexo;
+  private String raza;
+  private Double peso;
+  private String arre;
+  private int edad;
+  
+  /**
+   * Hace que el caballo relinche
+   */
+  void relincha() {
+    System.out.println("Hhiiiiiii");
+  }
+  
+  /**
+   * Hace que el caballo trote, galope o pare
+   */
+  void anda(String arre) {
+    if (arre.equals("galope")) {
+      System.out.println("al galooopeeee!!!");   
+    } else if (arre.equals("trote")) {
+      System.out.println("pues al trote");
+    } else {
+      System.out.println("no me muevo");
+    }
+  }
+  
+   //comer
+  void come(String comida){
+    if (comida.equals("zanahoria")) {
+      
+    }
+  }
+  
+}
---
+++ tema09/b_POOenJava/Ejercicio01.java
@@ -0,0 +1,27 @@
+package ejercicios.tema09.b_POOenJava;
+/**
+ * Tema 9 
+ * Ejercicio 1 probando Caballo.java
+ *
+ * Implementa la clase Caballo vista en un ejercicio anterior. Pruébala creando
+ * instancias y aplicándole algunos métodos
+ *
+ *
+ * @author Natalia Castillo Muñoz
+ */
+public class Ejercicio01 {
+  
+  public static void main(String[] args) {
+
+    Caballo rocinante = new Caballo();
+    
+    System.out.println("Hola caballo");
+    rocinante.relincha();
+    System.out.println("Vamos a dar una vuelta");
+    rocinante.anda("trote");
+    System.out.println("¿una carrera?");
+    rocinante.anda("galope");
+    
+
+  }
+}
