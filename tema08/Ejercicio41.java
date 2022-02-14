2d14f36c53c37b443b61240945eb04a48c985ab9
Natalia Castillo Muñoz <>
2022-02-15 00:29:47 +0100

    Ejercicios Tema 08

---
+++ tema08/Ejercicio41.java
@@ -0,0 +1,27 @@
+package tema08;
+
+import java.util.Scanner;
+
+public class Ejercicio41 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduce el carácter para dibujar: ");
+    char caracter = s.next().charAt(0);
+    System.out.print("Introduce la altura: ");
+    int repeticiones = s.nextInt();
+    System.out.println();
+    for (int i = repeticiones; i > 0; i--) {
+      linea(caracter, i);
+      System.out.println();
+    }
+    System.out.println();
+
+  }
+  public static void linea(char c, int r) {
+    for (int i = 0; i < r; i++) {
+      System.out.print(c);
+    }
+  }
+}
